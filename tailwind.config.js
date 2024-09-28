70b4ccf8e27d627c496e4cf190cbe9ea2586629f
Zachary Dorcinville <zdorcinville@gmail.com>
2024-09-28 17:13:51 -0400

    update theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,13 @@ module.exports = {
     extend: {
       colors: {
         primary: "#6a5acd",
+        blue: '#5072A7'
       },
+      gridTemplateColumns: {
+        'auto-fit-xl': 'repeat(auto-fit, 600px)',
+        'auto-fit-lg': 'repeat(auto-fit, 500px)',
+        'auto-fit-md': 'repeat(auto-fit, 400px),'
+      }
     },
     fontFamily: {
       titillium: ["Titillium Web"],
